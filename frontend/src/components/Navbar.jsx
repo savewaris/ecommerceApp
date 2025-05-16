d1d05b047bb3b15b1ed35da99a2276a57033031d
savewaris <savewaris@gmail.com>
2025-05-16 22:03:32 +0700

    route add

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { assets } from '../assets/frontend_assets/assets'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-5 font-medium'>
+      
+          <img src={assets.logo} className='w-36' alt="" />
+          
+          <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+              <NaLink className=' flex flex-col itemscenter gap-1'>
+                  
+              </NaLink>
+          </ul>
+    </div>
+  )
+}
+
+export default Navbar
